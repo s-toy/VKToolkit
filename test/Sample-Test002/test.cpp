365e27937dfb5d5dcf2261f5bc09e9734170b75d
Nie <yorknie1994@qq.com>
2019-06-05 14:08:36 +0800

    modified sample test 002

--- test/Sample-Test002/test.cpp
+++ test/Sample-Test002/test.cpp
@@ -1,68 +1,210 @@
 #include "pch.h"
+#include <vulkan/vulkan.hpp>
 #include "GLFW/glfw3.h"
 #include "VkContext.h"
 #include "VkSwapchain.h"
 
+#define CREATE_WINDOW glfwCreateWindow(800, 600, "Test_Swapchain", nullptr, nullptr)
+
+class Test_VkSwapchain :public ::testing::Test
+{
+public:
+	virtual void SetUp() override
+	{
+		hiveVKT::CVkContext::getInstance()->setPreferDiscreteGpuHint(true);
+		hiveVKT::CVkContext::getInstance()->setEnablePresentationHint(true);
+		hiveVKT::CVkContext::getInstance()->createContext();
+
+		glfwInit();
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+		m_pWindow = CREATE_WINDOW;
+
+		vk::SurfaceKHR pSurface = nullptr;
+		glfwCreateWindowSurface(hiveVKT::CVkContext::getInstance()->getVulkanInstance(), m_pWindow, nullptr, reinterpret_cast<VkSurfaceKHR*>(&pSurface));
+
+		vk::SurfaceCapabilitiesKHR SurfaceCapabilities = hiveVKT::CVkContext::getInstance()->getPhysicalDevice().getSurfaceCapabilitiesKHR(pSurface);
+		m_SupportedImageUsages = SurfaceCapabilities.supportedUsageFlags;
+
+		hiveVKT::CVkContext::getInstance()->getVulkanInstance().destroySurfaceKHR(pSurface);
+		hiveVKT::CVkContext::getInstance()->destroyContext();
+		glfwDestroyWindow(m_pWindow);
+		glfwTerminate();
+	}
+
+protected:
+	vk::ImageUsageFlags m_SupportedImageUsages = vk::ImageUsageFlags();
+	GLFWwindow* m_pWindow = nullptr;
+	hiveVKT::CVkSwapchain m_Swapchain;
+};
+
 //测试点：测试传入空窗口指针
 //
-TEST(Test_VkSwapchain, CreateSwapchainWithNullWindow)
+TEST_F(Test_VkSwapchain, CreateSwapchainWithNullWindow)
 {
+	hiveVKT::CVkContext::getInstance()->setPreferDiscreteGpuHint(true);
+	hiveVKT::CVkContext::getInstance()->setEnablePresentationHint(true);
+	hiveVKT::CVkContext::getInstance()->createContext();
 
+	m_Swapchain.createSwapchain(nullptr, m_SupportedImageUsages);
+	
+	m_Swapchain.destroySwapchain();
+	hiveVKT::CVkContext::getInstance()->destroyContext();
 }
 
-//测试点：测试传入正常的窗口指针，且其他的参数都合法
+//测试点：测试正常情况
 //
-TEST(Test_VkSwapchain, CreateSwapchainWithValidWindow)
+TEST_F(Test_VkSwapchain, CreateSwapchainWithValidWindow)
 {
+	hiveVKT::CVkContext::getInstance()->setPreferDiscreteGpuHint(true);
+	hiveVKT::CVkContext::getInstance()->setEnablePresentationHint(true);
+	hiveVKT::CVkContext::getInstance()->createContext();
 
+	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	m_pWindow = CREATE_WINDOW;
+
+	m_Swapchain.createSwapchain(m_pWindow, m_SupportedImageUsages);
+
+	m_Swapchain.destroySwapchain();
+	hiveVKT::CVkContext::getInstance()->destroyContext();
+	glfwDestroyWindow(m_pWindow);
+	glfwTerminate();
 }
 
 //测试点：测试重建swap chain
 //
-TEST(Test_VkSwapchain, RecreateSwapchain)
+TEST_F(Test_VkSwapchain, RecreateSwapchain)
 {
-
+	//TODO
 }
 
 //测试点：测试使用非法的image usage flags创建时，程序可以正常处理
 //
-TEST(Test_VkSwapchain, CreateWithInvalidImageUsageFlags)
+TEST_F(Test_VkSwapchain, CreateWithInvalidImageUsageFlags)
 {
+	hiveVKT::CVkContext::getInstance()->setPreferDiscreteGpuHint(true);
+	hiveVKT::CVkContext::getInstance()->setEnablePresentationHint(true);
+	hiveVKT::CVkContext::getInstance()->createContext();
 
+	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	m_pWindow = CREATE_WINDOW;
+
+	m_Swapchain.createSwapchain(m_pWindow, ~m_SupportedImageUsages);
+
+	m_Swapchain.destroySwapchain();
+	hiveVKT::CVkContext::getInstance()->destroyContext();
+	glfwDestroyWindow(m_pWindow);
+	glfwTerminate();
 }
 
 //测试点：测试重复调用create方法，程序可以正常处理
 //
-TEST(Test_VkSwapchain, DuplicateCreationCall)
+TEST_F(Test_VkSwapchain, DuplicateCreationCall)
 {
+	hiveVKT::CVkContext::getInstance()->setPreferDiscreteGpuHint(true);
+	hiveVKT::CVkContext::getInstance()->setEnablePresentationHint(true);
+	hiveVKT::CVkContext::getInstance()->createContext();
 
+	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	m_pWindow = CREATE_WINDOW;
+
+	m_Swapchain.createSwapchain(m_pWindow, m_SupportedImageUsages);
+	m_Swapchain.createSwapchain(m_pWindow, m_SupportedImageUsages);
+
+	m_Swapchain.destroySwapchain();
+	m_Swapchain.destroySwapchain();
+
+	hiveVKT::CVkContext::getInstance()->destroyContext();
+	glfwDestroyWindow(m_pWindow);
+	glfwTerminate();
 }
 
 //测试点：测试在未初始化Context之前创建swap chain时，程序可以正常工作
 //
-TEST(Test_VkSwapchain, CreateBeforeInitializeContext)
+TEST_F(Test_VkSwapchain, CreateBeforeInitializeContext)
 {
+	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
+	m_pWindow = CREATE_WINDOW;
+
+	m_Swapchain.createSwapchain(m_pWindow, m_SupportedImageUsages);
+	m_Swapchain.destroySwapchain();
 }
 
 //测试点：测试在不合适的上下文中创建swap chain，程序可以按预期处理
 //
-TEST(Test_VkSwapchain, CreateWithUnsuitableContext)
+TEST_F(Test_VkSwapchain, CreateWithUnsuitableContext)
 {
+	hiveVKT::CVkContext::getInstance()->setPreferDiscreteGpuHint(true);
+	hiveVKT::CVkContext::getInstance()->createContext();
 
+	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	m_pWindow = CREATE_WINDOW;
+
+	m_Swapchain.createSwapchain(m_pWindow, m_SupportedImageUsages);
+	
+	m_Swapchain.destroySwapchain();
+	hiveVKT::CVkContext::getInstance()->destroyContext();
+	glfwDestroyWindow(m_pWindow);
+	glfwTerminate();
 }
 
 //测试点：测试在已经销毁vulkan上下文后，进行swap chain的销毁，程序可按预期处理
 //
-TEST(Test_VkSwapchain, DestroyAfterDestructContext)
+TEST_F(Test_VkSwapchain, DestroyAfterDestructContext)
 {
+	hiveVKT::CVkContext::getInstance()->setPreferDiscreteGpuHint(true);
+	hiveVKT::CVkContext::getInstance()->setEnablePresentationHint(true);
+	hiveVKT::CVkContext::getInstance()->createContext();
 
+	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	m_pWindow = CREATE_WINDOW;
+
+	m_Swapchain.createSwapchain(m_pWindow, m_SupportedImageUsages);
+	
+	hiveVKT::CVkContext::getInstance()->destroyContext();
+	m_Swapchain.destroySwapchain();
+	glfwDestroyWindow(m_pWindow);
+	glfwTerminate();
 }
 
 //测试点：使用glfw时，会默认创建OpenGL上下文【可以通过：glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API)禁止】
 //		 本测试用例将测试，在此情况发生的情况下，程序可以正确处理
 //
-TEST(Test_VkSwapchain, CreateUnderOpenGLContext)
+TEST_F(Test_VkSwapchain, CreateUnderOpenGLContext)
 {
+	hiveVKT::CVkContext::getInstance()->setPreferDiscreteGpuHint(true);
+	hiveVKT::CVkContext::getInstance()->setEnablePresentationHint(true);
+	hiveVKT::CVkContext::getInstance()->createContext();
 
+	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	//glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	m_pWindow = CREATE_WINDOW;
+
+	m_Swapchain.createSwapchain(m_pWindow, m_SupportedImageUsages);
+
+	m_Swapchain.destroySwapchain();
+	hiveVKT::CVkContext::getInstance()->destroyContext();
+	glfwDestroyWindow(m_pWindow);
+	glfwTerminate();
 }
