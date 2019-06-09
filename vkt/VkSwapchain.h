e74d2daa6ba856f3ebd95aa5c64fca494488fe77
Nie <yorknie1994@qq.com>
2019-06-09 21:49:34 +0800

    modified sample test 002

--- vkt/VkSwapchain.h
+++ vkt/VkSwapchain.h
@@ -11,19 +11,27 @@ namespace hiveVKT
 		CVkSwapchain();
 		~CVkSwapchain();
 
-		void createSwapchain(GLFWwindow* vGLFWwindow, vk::ImageUsageFlags vImageUsageFlags = vk::ImageUsageFlagBits::eColorAttachment, vk::SwapchainKHR vOldSwapchain = nullptr);
+		bool createSwapchain(GLFWwindow* vGLFWwindow, vk::ImageUsageFlags vImageUsageFlags = vk::ImageUsageFlagBits::eColorAttachment, vk::SwapchainKHR vOldSwapchain = nullptr);
 		void destroySwapchain();
 
-		const vk::SurfaceKHR& getSurface()const { _ASSERT(m_IsInitialized); return m_pSurface; }
-		const vk::SwapchainKHR& getSwapchain()const { _ASSERT(m_IsInitialized); return m_pSwapChain; }
+		const vk::SurfaceKHR& getSurface()const { return m_pSurface; }
+		const vk::SwapchainKHR& getSwapchain()const { return m_pSwapChain; }
 
-		const vk::Image& getSwapchainImageAt(int vIndex)const { _ASSERT(m_IsInitialized && vIndex >= 0 && vIndex < m_SwapchainImages.size()); return m_SwapchainImages[vIndex]; }
-		const vk::ImageView& getSwapchainImageViewAt(int vIndex)const { _ASSERT(m_IsInitialized && vIndex >= 0 && vIndex < m_SwapchainImages.size()); return m_SwapchainImageViews[vIndex]; }
+		const vk::Image& getSwapchainImageAt(size_t vIndex)const
+		{
+			_ASSERT(m_IsInitialized && vIndex >= 0 && vIndex < m_SwapchainImages.size());
+			return m_SwapchainImages[vIndex];
+		}
+		const vk::ImageView& getSwapchainImageViewAt(size_t vIndex)const
+		{
+			_ASSERT(m_IsInitialized && vIndex >= 0 && vIndex < m_SwapchainImages.size());
+			return m_SwapchainImageViews[vIndex];
+		}
 
 		const vk::Format& getSwapchainImageFormat() const { return m_SwapchainImageFormat; }
 		const vk::Extent2D& getSwapchainImageExtent() const { return m_SwapchainImageExtent; }
 
-		size_t getNumSwapchainImage()const { _ASSERT(m_IsInitialized); return m_SwapchainImages.size(); }
+		size_t getNumSwapchainImage()const { return m_SwapchainImages.size(); }
 
 	private:
 		bool m_IsInitialized = false;
