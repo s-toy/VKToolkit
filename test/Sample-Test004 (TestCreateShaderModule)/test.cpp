6a24372949fd1de02cb9a6becd7b601de646e74d
sujiangshan <1549390598@qq.com>
2019-06-16 18:56:03 +0800

    modified test

--- test/Sample-Test004 (TestCreateShaderModule)/test.cpp
+++ test/Sample-Test004 (TestCreateShaderModule)/test.cpp
@@ -29,17 +29,9 @@ protected:
 	{
 		hiveVKT::CVkCallParser Parser;
 		_ASSERT(Parser.parse("vk_apidump.txt"));
-		const auto& ParseResultSet = Parser.getVKCallInfoAt(0, 0);
-		int Counter = 0;
-		for (auto VkCall : ParseResultSet)
-		{
-			if (VkCall.FunctionName == "vkCreateShaderModule")
-			{
-				EXPECT_TRUE(VkCall.ReturnValue == "VK_SUCCESS");
-				Counter++;
-			}
-		}
-		return Counter;
+		auto CreateShaderModuleCallInfo = Parser.getVkCallInfoByFunctionName(0, 0, "vkCreateShaderModule");
+
+		return CreateShaderModuleCallInfo.size();
 	}
 
 	const CVkDebugUtilsMessenger* m_pMessenger = nullptr;
